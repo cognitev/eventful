dd7207935c91ee22bbb9fb555fb3a9e97a6db83e
Mahmoud Afify <mahmoud.afify@cognitev.com>
2018-11-26 17:00:55 +0200

    integrate travis

---
+++ config/travis.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :eventful, EventfulWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :eventful, Eventful.Repo,
+  username: "root",
+  password: System.get_env("DB_PASSWORD"),
+  database: "eventful_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
