9134dcc213450a4456b7511b0dc02997c0049aed
Mahmoud Afify <mahmoud.afify@cognitev.com>
2018-12-17 15:34:02 +0200

    update

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ set -x
 
 if [ "$1" = "eventful" ];
 then
+
     echo "==================="
     echo "Running Eventful"
     echo "==================="
     echo
     /usr/local/bin/envconsul -consul consul.mcagrid.com -token $CONSUL_TOKEN -prefix $DEPLOY_ENV/ mix ecto.migrate
     /usr/local/bin/envconsul -consul consul.mcagrid.com -token $CONSUL_TOKEN -prefix $DEPLOY_ENV/ mix phx.server
+
 fi
